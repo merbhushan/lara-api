c5d69af3d99700e58afeb348f6c77a10b9b18a3b
Bhushan Mer <bhushanmer@gmail.com>
2018-10-17 15:27:04 +0530

    Scope is added to exclue hidden type fields

--- app/Model/DataRow.php
+++ app/Model/DataRow.php
@@ -5,6 +5,9 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class DataRow extends Model
-{
-    //
+{	
+	// Scope is added to exclue hidden type fields
+    public function scopeSkipHidden($query){
+    	return $query->where('element_type_id', '<>', 0);
+    }
 }
