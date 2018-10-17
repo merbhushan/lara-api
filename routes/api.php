cdde746e27621cba6ac0dc0c4280aa97e02e17ac
Bhushan Mer <bhushanmer@gmail.com>
2018-10-17 13:39:42 +0530

    Generate routing using database

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,36 @@ use Illuminate\Http\Request;
 Route::get('error/{error}', 'exceptoinController@index');
 
 // InitialiseSession middleware is being added to store global information into a session of a user once token is being validate by passport
-Route::middleware(['auth:api', 'ValidateUserAccess', 'InitialiseSession'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api', 'ValidateUserAccess', 'InitialiseSession'])->group(function(){
+	$arrMiddleware	=	[];
+	foreach (Scope::where('is_active', 1)->get() as $scope) {
+		$dataType = $scope->dataType;
+		
+		// Fetch action controller. Default is breadController
+		$apiController = $dataType->controller
+	                            ? $scope->controller
+	                            : 'breadController';
+	    // Fetch Scope name
+	    $strApiScopes = $scope->name;
+
+	    $arrResourceAction	=	$scope->apiAction()->select('name')->get()->pluck('name')->toArray();
+	    
+	    // Custom validation middleware
+	    if(!empty($scope->middleware)){
+	        $arrMiddleware[]	=	$scope->middleware;
+	    }
+
+	    // scope validattion middleware
+	    if(!empty($strApiScopes)){
+        	$arrMiddleware[]	=	'scope:' .$strApiScopes;
+        }
+
+        // If there is any middleware for a route then add it with a route.
+        if(empty($arrMiddleware)){
+        	Route::resource($dataType->slug, $apiController, ["only"=>$arrResourceAction, "as" =>$strApiScopes]);
+        }
+        else{
+        	Route::resource($scope->slug, $apiController, ["only"=>$arrResourceAction])->middleware($arrMiddleware);
+        }
+	}
 });
