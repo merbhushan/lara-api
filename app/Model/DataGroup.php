7b76f9c78e4cfb2c076c8907f68b60b6f0ddeb07
Bhushan Mer <bhushanmer@gmail.com>
2018-10-17 15:24:13 +0530

    One to many relationship with DataRow

--- app/Model/DataGroup.php
+++ app/Model/DataGroup.php
@@ -5,6 +5,9 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class DataGroup extends Model
-{
-    //
+{	
+	// One to many relationship with DataRow
+    public function dataRow(){
+    	return $this->hasMany('App\Model\DataRow');
+    }
 }
