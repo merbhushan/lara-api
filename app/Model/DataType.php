d973a47503b9332736ba52dfe090ebbd542cc04b
Bhushan Mer <bhushanmer@gmail.com>
2018-10-17 15:25:11 +0530

    Relationship with ApiAction

--- app/Model/DataType.php
+++ app/Model/DataType.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataType extends Model
 {
-    //
+	// Relationship with ApiAction
+    public function apiAction(){
+    	return $this->belongsToMany('App\Model\ApiAction', 'data_type_api_action');
+    }
 }
