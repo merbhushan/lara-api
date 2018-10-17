9caaf6b1029536af86d868ce8db88410e16a5d69
Bhushan Mer <bhushanmer@gmail.com>
2018-10-17 15:47:05 +0530

    Http Response method is being added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public static function httpResponse($data, $code=1, $intResponseCode = 200, $type='application/json', $strAllowOrigin = '*'){
+    	
+    	if(!empty($data)){
+        	$arrData = is_array($data)?$data:$data->toArray();    		
+    	}
+    	else{
+    		$code = '';
+    		$intResponseCode = 404;
+    		$arrData = '';
+    	}
+
+        $arrResponse = [
+            "code" => $code,
+            "data" => $arrData
+        ];
+        
+        return response(json_encode($arrResponse), $intResponseCode)->header('content-type', $type);
+    }
 }
