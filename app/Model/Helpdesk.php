e52239e9c13397e80e70f7c48eaead4a4ed0e968
Bhushan Mer <bhushanmer@gmail.com>
2018-10-26 15:50:38 +0530

    Relationship Added: Assign To & Task

--- app/Model/Helpdesk.php
+++ app/Model/Helpdesk.php
@@ -9,4 +9,12 @@ class Helpdesk extends Model
     public function createdBy(){
     	return $this->belongsTo('App\User', 'created_by');
     }
+
+    public function assignTo(){
+    	return $this->belongsToMany('App\User', 'helpdesk_assign_to', 'helpdesk_id', 'user_id');
+    }
+
+    public function tasks(){
+    	return $this->hasMany('App\Model\Task');
+    }
 }
