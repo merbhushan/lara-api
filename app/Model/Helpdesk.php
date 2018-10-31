8ad960680ef2c6381536389634c57c077fc62424
Bhushan Mer <bhushanmer@gmail.com>
2018-10-31 13:55:57 +0530

    Soft Delete enabled.

--- app/Model/Helpdesk.php
+++ app/Model/Helpdesk.php
@@ -3,9 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Helpdesk extends Model
 {
+	use SoftDeletes;
     public function createdBy(){
     	return $this->belongsTo('App\User', 'created_by');
     }
