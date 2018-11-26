7ecc8d38bc7c08d119995d045c4bf34ca81210ee
Bhushan Mer <bhushanmer@gmail.com>
2018-11-26 15:57:20 +0530

    Bug Fix: Relationship object should be key by relationship_id insted of id.

--- app/Http/Controllers/breadController.php
+++ app/Http/Controllers/breadController.php
@@ -77,7 +77,7 @@ class breadController extends Controller
 
         if($objFields->count()){
             // Get Relationships 
-            $objRelationships = $this->objUserAccess->dataType->relationships->keyBy('id');
+            $objRelationships = $this->objUserAccess->dataType->relationships->keyBy('relationship_type_id');
             // Initialize variables for fields.
             $strRawFields = '';
             $strFields = '';
