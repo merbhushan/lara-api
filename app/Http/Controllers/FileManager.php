0855ce6f88d6dd9628c5c13d92675d3b8d0ba924
Bhushan Mer <bhushanmer@gmail.com>
2018-12-03 15:03:43 +0530

    Controller created to manage Storage.

---
+++ app/Http/Controllers/FileManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+
+/**
+ * This class is being used to manage storage.
+ */
+class FileManager extends Controller
+{
+    
+}
