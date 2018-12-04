343c6363fa61b235f0f8c35dd5f8f1a50aba9d78
Bhushan Mer <bhushanmer@gmail.com>
2018-12-04 15:25:07 +0530

    Has many relationship added with items.

--- app/Model/Attachment.php
+++ app/Model/Attachment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attachment extends Model
 {
-    //
+	// Attachment can map with multiple items.
+	public function AttachmentMapping(){
+		return $this->hasMany('App\Model\AttachmentMapping');
+	}
 }
