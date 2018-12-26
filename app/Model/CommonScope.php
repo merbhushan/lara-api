1ab98c2b61eea8766b2d84581c93970812138fa4
Bhushan Mer <bhushanmer@gmail.com>
2018-12-26 18:03:40 +0530

    Commenting hasOne Relationship Added: attachment.

--- app/Model/CommonScope.php
+++ app/Model/CommonScope.php
@@ -4,14 +4,15 @@ namespace App\Model;
 
 trait CommonScope
 {
+    // Active Scope
     public function scopeActive($query){
         return $query->where('is_active', 1);
     }
-
+    // Filter on scope name
     public function scopeName($query, $strName){
     	return $query->where('name', $strName);
     }
-
+    // Scope on user_id
     public function scopeUser($query, $strUsers){
     	if(empty($strUsers)){
     		$strUsers = session('user_id', null);
@@ -22,5 +23,13 @@ trait CommonScope
     	
     	return $query->whereIn('user_id', $strUsers);
     	
+    }
+    // hasOne Relationship for attachment.
+    public function attachment(){
+        return $this->hasOne('App\Model\AttachmentMapping', 'module_ref_id')
+            ->with(['attachment' => function($query){
+                $query->selectRaw('id, upload_path, mime_type, original_name, "" as view, "" as download')
+                ->where('status', 2);
+            }]);
     }
 }
