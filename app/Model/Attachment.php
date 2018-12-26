d5de560519c83c27be17af35535c62e9d70e16ec
Bhushan Mer <bhushanmer@gmail.com>
2018-12-26 18:04:12 +0530

    Accessor created for view & download.

--- app/Model/Attachment.php
+++ app/Model/Attachment.php
@@ -3,11 +3,25 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\FileManager;
 
 class Attachment extends Model
 {
+	public $objFileManager;
+
+	public function __construct(){
+		$this->objFileManager = new FileManager();
+	}
 	// Attachment can map with multiple items.
 	public function AttachmentMapping(){
 		return $this->hasMany('App\Model\AttachmentMapping');
+	}
+
+	public function getViewAttribute($value){
+		return $this->objFileManager->getFile($this, 0, 1, 0);
+	}
+
+	public function getDownloadAttribute($value){
+		return $this->objFileManager->getFile($this, 1, 1, 0);
 	}
 }
