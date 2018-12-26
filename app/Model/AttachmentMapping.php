8d6aef9fc0a47e839a12ff6a8481c680d4078020
Bhushan Mer <bhushanmer@gmail.com>
2018-12-26 18:07:04 +0530

    Soft delete & user modification attributes added.

--- app/Model/AttachmentMapping.php
+++ app/Model/AttachmentMapping.php
@@ -3,8 +3,19 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AttachmentMapping extends Model
 {
     protected $table = 'attachment_mapping';
+
+    use SoftDeletes;
+
+    public $blnStoreUserInfo=1;
+	public $blnUpdateDeleteByInfo =1;
+
+    public function attachment(){
+    	return $this->belongsTo('App\Model\Attachment');
+    }
+
 }
